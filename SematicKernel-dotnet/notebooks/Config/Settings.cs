fb1e85b0e428c715d3cc20fc91ee72cb44f95494
Ruslan Shpileuski <Ruslan_Shpileuski@epam.com>
2023-12-09 21:26:02 +0300

    fix: practical task 1 changes

--- SematicKernel-dotnet/notebooks/Config/Settings.cs
+++ SematicKernel-dotnet/notebooks/Config/Settings.cs
@@ -11,15 +11,21 @@ public static class Settings
     private const string DefaultModel = "gpt-35-turbo";
     private const string ModelKey = "model";
     private const string SecretKey = "apikey";
-    private const string OrgKey = "org";
+    private const string ServiceIdKey = "serviceid";
     private const string EndpointKey = "endpoint";
 
-    public static (string model, string azureEndpoint, string apiKey, string orgId) LoadFromFile(
+    public static (string model, string azureEndpoint, string apiKey, string serviceId) LoadFromFile(
         string configFile = DefaultConfigFile,
         string model = DefaultModel)
     {
         try
         {
+            if (string.IsNullOrEmpty(configFile) || !File.Exists(configFile))
+            {
+                Console.WriteLine("Invalid or missing configuration file path.");
+                throw new ArgumentException("Invalid or missing configuration file path", nameof(configFile));
+            }
+
             string json = File.ReadAllText(configFile);
             var settingsList = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(json);
 
@@ -37,17 +43,18 @@ public static class Settings
                 throw new Exception($"Configuration for model '{model}' not found");
             }
 
-            string loadedModel = GetValueOrDefault(config, ModelKey);
-            string azureEndpoint = GetValueOrDefault(config, EndpointKey);
-            string apiKey = GetValueOrDefault(config, SecretKey);
-            string orgId = GetValueOrDefault(config, OrgKey);
+            // Validate and retrieve values
+            string loadedModel = ValidateAndGet(config, ModelKey);
+            string azureEndpoint = ValidateAndGet(config, EndpointKey);
+            string apiKey = ValidateAndGet(config, SecretKey);
+            string serviceId = ValidateAndGet(config, ServiceIdKey);
 
-            if (orgId == "none")
+            if (serviceId == "none")
             {
-                orgId = "";
+                serviceId = "";
             }
 
-            return (loadedModel, azureEndpoint, apiKey, orgId);
+            return (loadedModel, azureEndpoint, apiKey, serviceId);
         }
         catch (Exception e)
         {
@@ -56,8 +63,14 @@ public static class Settings
         }
     }
 
-    private static string GetValueOrDefault(Dictionary<string, string> config, string key)
+    private static string ValidateAndGet(Dictionary<string, string> config, string key)
     {
-        return config.TryGetValue(key, out var value) ? value : "";
+        if (!config.TryGetValue(key, out var value) || string.IsNullOrEmpty(value))
+        {
+            Console.WriteLine($"Invalid or missing value for key '{key}'.");
+            throw new ArgumentException($"Invalid or missing value for key '{key}'", nameof(key));
+        }
+
+        return value;
     }
 }
