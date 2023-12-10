7f137694e07019d41f77e7bb8c173f231a66996f
Ruslan Shpileuski <Ruslan_Shpileuski@epam.com>
2023-12-10 19:34:14 +0300

    impl: split up beta and rc versions into separate files

--- SematicKernel-dotnet/notebooks/Plugins/CustomPlugin/CustomPlugin.cs
+++ SematicKernel-dotnet/notebooks/Plugins/CustomPlugin/CustomPlugin.cs
@@ -21,21 +21,24 @@ public class CustomPlugin
         _serviceId = serviceId;
     }
 
-    [SKFunction, Description("Initiates a chat with the ChatBot.")]
+    [SKFunction, Description("Initiates a chat with the Bot.")]
     public async Task<string> ChatAsync(SKContext context)
     {
         var prompt = @"
         Today is: {{time.Date}}
         Current time is: {{time.Time}}
 
-        ChatBot can have a conversation with you about any topic.
-            It can give detailed answer or say 'I don't know' if it does not have an answer.
+        Bot can have a conversation with you about any topic.
+        It can give detailed answer or say 'I don't know' if it does not have an answer.
 
-            {{$history}}
-            User: {{$message}}
-            ChatBot:";
+        {{$history}}
+
+        User: {{$message}}
+        Bot:";
 
         var renderedPrompt = await _promptTemplateFactory.Create(prompt, new PromptTemplateConfig()).RenderAsync(context);
+
+        Console.WriteLine($"[Prompt]: {renderedPrompt}");
 
         var skFunction = _kernel.CreateSemanticFunction(
             promptTemplate: renderedPrompt,
@@ -51,16 +54,20 @@ public class CustomPlugin
 
         var result = await skFunction.InvokeAsync(context: context, requestSettings: requestSettings);
         var resultAsString = result.GetValue<string>();
+
         // Append the new interaction to the chat history
         string history = context.Variables["history"];
         history += $"\nUser: {context.Variables["message"]}\nSuggestions: {resultAsString}\n";
-        return history;
+        context.Variables["history"] = history;
+
+        return resultAsString;
     }
 
     [SKFunction, Description("Initiates a translation task with the ChatBot by requesting the translation of a given text to a specified language.")]
     public async Task<string> TranslateAsync(SKContext context)
     {
-        var prompt = @"You are the best linguist. Please translate the given TEXT to {{$lang}}. TEXT: {{$Input}}";
+        var prompt = @"You are the best linguist. Please translate the given TEXT to {{$lang}}. 
+        TEXT: {{$Input}}";
 
         var renderedPrompt = await _promptTemplateFactory.Create(prompt, new PromptTemplateConfig()).RenderAsync(context);
 
